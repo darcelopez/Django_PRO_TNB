f1d16a0494e045c293520e3ec06607a4764d4236
Dennis <dennis.arcelopez@gmail.com>
2023-05-29 22:18:23 -0400

    Initial commit 02
    -Settings moved to separate module
    -Created extra files for settings handleing

---
+++ core_auth/core/project/settings/envvars.py
@@ -0,0 +1,11 @@
+# from auth_core.general.utils.collections import deep_update
+# from auth_core.general.utils.settings import get_settings_from_environment
+
+"""
+This takes env variables with a matching prefix, strips out the prefix, and adds it to globals
+"""
+
+# globals() is a dictionary of global variables
+# deep_update(globals(), get_settings_from_environment(ENVVAR_SETTINGS_PREFIX))
+
+print('finish envvars.py')
